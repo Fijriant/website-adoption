3c8df7eebda8673dfec2c011cf32279a4e59c725
Fijriant <160816946+Fijriant@users.noreply.github.com>
2025-01-10 11:09:39 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import dynamic from "next/dynamic";
 
-import Hero from "./components/hero";
-import About from "./components/about";
-import Project from "./components/project";
-import Partner from "./components/partner";
-import Footer from "./components/footer";
-
+// Mengimpor komponen Hero dengan ssr: false
+const Hero = dynamic(() => import("./components/hero"), { ssr: false });
+const About = dynamic(() => import("./components/about"), { ssr: false });
+const Project = dynamic(() => import("./components/project"), { ssr: false });
+const Partner = dynamic(() => import("./components/partner"), { ssr: false });
+const Footer = dynamic(() => import("./components/footer"), { ssr: false });
 
 export default function Home() {
   return (
